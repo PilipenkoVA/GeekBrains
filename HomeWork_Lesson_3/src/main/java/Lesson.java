a4c15a7bd411ee3941a3dec311cb5b69c4d4858c
Vitaliy <Pilipenko.va@live.ru>
2021-02-13 18:39:06 +0300

    Update Lesson.java

--- HomeWork_Lesson_3/src/main/java/Lesson.java
+++ HomeWork_Lesson_3/src/main/java/Lesson.java
@@ -7,13 +7,24 @@ public class Lesson {
         System.out.println("Ваша задача угадать число");
         int range = 9;
         int number = (int) (Math.random() * range);
+        playLevel(range, number);
 
-
+        scanner.close();
 
     }
     private static void playLevel (int range, int number){
-        while (true){
+        while (true){// Заменить бесконечный цикл на 3 попытки
             System.out.println("Угадайте число от 0 до " + range);
+            int input_number = scanner.nextInt();
+            if (input_number == number){
+                System.out.println("Вы угадали");
+                // заменить на бесконечный цикл игр но с выводом сообщение "Выхотите продолжить игру" (да)(нет)
+                break;
+            }else if (input_number < number){
+                System.out.println("Данное число меньше");
+            }else {
+                System.out.println("Данное число больше");
+            }
         }
     }
 }
