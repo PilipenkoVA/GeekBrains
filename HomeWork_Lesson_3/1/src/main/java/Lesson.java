e9426aaa1da62e6c226f31be0bde47725191a18d
Vitaliy <Pilipenko.va@live.ru>
2021-02-15 23:04:49 +0300

    Update Lesson.java

--- HomeWork_Lesson_3/1/src/main/java/Lesson.java
+++ HomeWork_Lesson_3/1/src/main/java/Lesson.java
@@ -6,6 +6,48 @@ public class Lesson {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
+        playLevelOne();
+//        playLevelTwo();
+        scanner.close();
+
+    }
+    // 1 задание:
+
+    private static void playLevelOne (){
+        System.out.println("Ваша задача угадать число");
+        int n = 3;
+        int range = 9;
+        int number = (int) (Math.random() * range);
+        while (n > 0){
+            System.out.println("Угадайте число от 0 до " + range);
+            System.out.println("У вас  " + n + " попытки");
+            int input_number = scanner.nextInt();
+            if (input_number == number){
+                System.out.println("Вы угадали");
+            }else if (input_number < number){
+                System.out.println("Данное число меньше");
+                n--;
+            }else {
+                System.out.println("Данное число больше");
+                n--;
+            }if (n == 0 || input_number == number){
+                int a = 0;
+                int b = 1;
+                System.out.println("THE END");
+                System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
+                int input_level = scanner.nextInt();
+                if (input_level == a){
+                    break;
+                }else {
+                    playLevelOne();
+                }
+            }
+        }
+    }
+    // 2 задание:
+
+    private static void playLevelTwo(){
         String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
                 "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut",
                 "pear", "pepper", "pineapple", "pumpkin", "potato"};
