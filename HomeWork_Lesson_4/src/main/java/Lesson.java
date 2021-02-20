b464f4b7f499258ec9bd2fef98fc17bcdcfbf5bf
Vitaliy <Pilipenko.va@live.ru>
2021-02-20 23:07:30 +0300

    first

---
+++ HomeWork_Lesson_4/src/main/java/Lesson.java
@@ -0,0 +1,14 @@
+public class Lesson {
+
+    public static void main(String[] args) {
+        private static final int size_X = 6;
+        private static final int size_Y = 5;
+        private static final char playerDOT = 'X';
+        private static final char compDOT = '0';
+        private static final char emptyDOT = '.';
+        private static final int toWin = 3;
+
+        private static char[][] field = new char[size_X][size_Y];
+    }
+}
+
