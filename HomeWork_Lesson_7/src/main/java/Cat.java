27aa323ff19307ad04418c88d86ea35459dd0087
Vitaliy <Pilipenko.va@live.ru>
2021-03-04 23:40:49 +0300

    ДЗ

--- HomeWork_Lesson_7/src/main/java/Cat.java
+++ HomeWork_Lesson_7/src/main/java/Cat.java
@@ -1,3 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Cat {
+
+    private String name;
+    private int appetite;
+    private boolean full; // "boolean default = false" - поэтому кот при создании "голодный"
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public int getAppetite() {
+
+        return appetite;
+    }
+
+    public void eat(Plate plate) {
+
+        full = plate.decreaseFood(appetite);
+    }
+
+    public void info() {
+
+        System.out.println(name + " " + (full ? " - наелся." : " - остался голоден так как ему не хватило еды."));
+    }
+
 
 }
--- HomeWork_Lesson_7/src/main/java/Plate.java
+++ HomeWork_Lesson_7/src/main/java/Plate.java
@@ -1,2 +1,39 @@
 public class Plate {
+
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public boolean decreaseFood(int appetite) {
+        if (food - appetite < 0) {
+            //4. Считаем, что если коту мало еды в тарелке, то он ее просто не трогает, то есть не может быть наполовину сыт (это сделано для упрощения логики программы)
+            return false;
+        }
+        else {
+            food -= appetite;
+            return true;
+        }
+    }
+    public void inInfo() {
+
+        System.out.println("В тарелку насыпали " + food + " грамм. корма.\nПосле чего решили покормить котов:");
+    }
+
+    public void offInfo() {
+
+        System.out.println("\nВ тарелке осталось " + food + " грамм. корма.");
+    }
+
+    //6. Добавить в тарелку метод, с помощью которого можно было бы добавлять еду в тарелку
+    public void addFood(int addFood) {
+        this.food += addFood;
+    }
+
+
 }
