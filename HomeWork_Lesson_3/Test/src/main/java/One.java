14f4f1f1ffb4bc27ed9bc6ce4aeb9a671a1aeb8d
Vitaliy <Pilipenko.va@live.ru>
2021-03-13 15:46:53 +0300

    one

---
+++ HomeWork_Lesson_3/Test/src/main/java/One.java
@@ -0,0 +1,9 @@
+public class One {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++){
+            if (i % 4 == 0){
+                System.out.println("p= "+ i);
+            }
+        }
+    }
+}
